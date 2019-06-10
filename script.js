768972adc022b64d21f404aea3bd4e7e187d0d6c
cis101 <cis101@secs.iub.edu.bd>
2019-06-10 09:10:42 +0600

    task2a

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var element = document.getElementById('heading')
 console.log(element.innerText)
 
 /* Task 2 using plain JavaScript */
+var textBtn = document.getElementById('addText1')
+
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "hello world"
+    task2.appendChild(paragraph)
+})
+
 
 
 
