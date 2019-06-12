150b058b51c1d3138303a83689b97de99b01cd8a
cis101 <cis101@secs.iub.edu.bd>
2019-06-12 09:10:08 +0600

    task4

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ function redirectToGoogle(event){
     window.location.assign('http://www.google.com')
 }
 /* Task 4 using jQuery */
+
+//task 4a
+$('#addText2').click(function(event){
+   
+    var paragraph = $('<p>').text('Hello World')
+    $('#task4a').append(paragraph)
+})
+
+// task 4b
+function changeBG(event){
+    var color = event.target.innerText
+    $('body').css('background-color',color)
+}
+// task 4c 
+
+function Add(event){
+ var sum = Number($('#Num1').val()) 
+            + Number($('#Num2').val())
+        $('#sum').text(sum)
+}
